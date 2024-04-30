6af48cfc803a3790e2e007dbe2a8122e388d5c26
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-04-30 19:44:59 +0530

    create product model

---
+++ api/models/product.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a product name"],
+      maxLength: [200, "Product name cannot exceed 200 characters"],
+    },
+    price: {
+      type: Number,
+      required: [true, "please enter product price"],
+      maxLength: [5, "price cannot exceed 5 digits"],
+    },
+    description: {
+      type: String,
+      required: [true, "please provide product description"],
+    },
+    ratings: {
+      type: Number,
+      default: 0,
+    },
+    images: [
+      {
+        public_id: {
+          type: String,
+          required: true,
+        },
+        url: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    category: {
+      type: String,
+      required: [true, "Please enter product category"],
+    },
+    stock: {
+      type: Number,
+      required: [true, "Please enter product stock"],
+    },
+    numOfReviews: {
+      type: Number,
+      default: 0,
+    },
+    reviews: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        rating: {
+          type: Number,
+          required: true,
+        },
+        comment: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Product", productSchema);
