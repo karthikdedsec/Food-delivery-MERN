68161e46195cae43f335f8ebc10b20335cf655ef
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-05-04 13:52:48 +0530

    handle unhandled promise rejection and catch async errors

--- api/app.js
+++ api/app.js
@@ -5,6 +5,14 @@ import { connectDatabase } from "./config/dbConnect.js";
 import errorMiddleware from "./middlewares/error.js";
 
 const app = express();
+
+//handle uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err}`);
+  console.log("shutting down due to uncaught exception");
+  process.exit(1);
+});
+
 dotenv.config({ path: "config/config.env" });
 
 //connect database
@@ -19,4 +27,13 @@ app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   console.log(`server running on port ${process.env.PORT}`);
+});
+
+//handle unhandled promise rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err}`);
+  console.log("shutting down server due to unhandled promise rejection");
+  server.close(() => {
+    process.exit(1);
+  });
 });
