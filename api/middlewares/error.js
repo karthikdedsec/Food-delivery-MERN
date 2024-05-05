c426ca1c20757ec7693a9d0ce791ab9a7ca782b4
karthik Raj <61104188+karthikdedsec@users.noreply.github.com>
2024-05-05 18:51:13 +0530

    handle validation and mongoose id error

--- api/middlewares/error.js
+++ api/middlewares/error.js
@@ -1,10 +1,25 @@
+import ErrorHandler from "../utils/errorHandler.js";
+
 export default (err, req, res, next) => {
   let error = {
     statusCode: err?.statusCode || 500,
     message: err?.message || "Internal server error",
   };
 
+  //handle invalid mongoose id error
+  if (err.name === "CastError") {
+    const message = `Resource not found.Invalid: ${err?.path}`;
+    error = new ErrorHandler(message, 404);
+  }
+  //handle validation error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((value) => value.message);
+    error = new ErrorHandler(message, 404);
+  }
+
   res.status(error.statusCode).json({
     message: error.message,
+    error: err,
+    stack: err?.stack,
   });
 };
